3dea504784aaef2cbef10d6e51dcffda995c835d
frostyosty <92064099+frostyosty@users.noreply.github.com>
2023-07-25 17:29:36 +1200

    pub key

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ document.getElementById('contactForm').addEventListener('submit', function (even
     }
   
     const formData = {
-      name: document.getElementById('name').value,
-      email: document.getElementById('email').value,
+      from_name: document.getElementById('name').value,
+      from_email: document.getElementById('email').value,
       subject: document.getElementById('subject').value,
       message: document.getElementById('message').value,
     };
